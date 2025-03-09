cdc8e30b2bca69e0f435ec362182824a4c15d364
Salihay06 <salihay.mardin47@gmail.com>
2025-03-09 03:55:07 +0300

    Add files via upload

---
+++ Employee salary/Employee/src/Employee.java
@@ -0,0 +1,36 @@
+public class Employee {
+    public String name;
+    public double salary;
+    public int workHours,hireYear;
+
+    Employee(int hireYear,int workHours,double salary, String name){
+     this.name=name;
+     this.salary=salary;
+     this.hireYear=hireYear;
+     this.workHours=workHours;   
+    }
+
+    public double tax(){
+        if (this.salary>=1000){
+            return salary*0.03;
+        }
+        return 0.0;
+    }
+    public double bonus(){
+        int extraHours=this.workHours-40;
+        if(extraHours>0){
+            return 30*extraHours;
+        }
+        return 0.0;
+    }
+    public double increase(){
+        int year=2020-this.hireYear;
+        if(year<10){
+            return salary*0.5;
+        }else if(year>=10 && year<20){
+            return salary*0.10;
+        }else{
+            return salary*0.15;
+        }
+    }
+}
